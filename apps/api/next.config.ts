3bf8aeb9eb26d820c4897efcdd87ff392a6b02d6
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2025-04-16 23:42:21 +0600

    Update CORS headers in next.config.ts to enhance security and support additional methods and headers

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -11,14 +11,16 @@ const nextConfig: NextConfig = {
       {
         source: '/:path*', // Or any other route you want to enable CORS for
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // Or specify a specific origin
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
           },
         ],
       },
