c8e637d84fafe0d10e01ae4fcec267f6438ec159
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2025-04-23 12:01:26 +0600

    feat: create JournalWritePage component to integrate the Editor

---
+++ apps/app/src/app/(protected)/@user/journal/write/page.tsx
@@ -0,0 +1,9 @@
+import { Editor } from './_components/editor';
+
+const JournalWritePage = () => {
+  return <Editor />;
+};
+
+JournalWritePage.displayName = 'JournalWritePage';
+
+export default JournalWritePage;
