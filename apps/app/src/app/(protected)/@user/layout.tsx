f7bfe815c7436525cbc48603338da46df7a33d6d
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2025-05-03 22:20:59 +0600

    feat: enhance UserDashboardLayout with vault checking logic and async session handling

--- apps/app/src/app/(protected)/@user/layout.tsx
+++ apps/app/src/app/(protected)/@user/layout.tsx
@@ -1,18 +1,33 @@
 import DynamicBreadcrumbs from '@/components/dynamic-breadcrumbs';
+import { auth } from '@repo/auth';
 import { Separator } from '@repo/design-system/components/ui/separator';
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from '@repo/design-system/components/ui/sidebar';
+import axios from 'axios';
 import type React from 'react';
 import { AppSidebar } from './_components/app-sidebar';
+import { VaultCheck } from './_components/vault-check';
 
 interface UserDashboardLayoutProps {
   children: React.ReactNode;
 }
 
-const UserDashboardLayout = ({ children }: UserDashboardLayoutProps) => {
+const UserDashboardLayout = async ({ children }: UserDashboardLayoutProps) => {
+  const session = await auth();
+  const user = session?.user;
+  const userId = user?.id || null;
+
+  const hasVaults = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/v1/user/${userId}/check-vaults`
+  );
+
+  if (!hasVaults.data.hasVaults) {
+    return <VaultCheck userId={userId}>{children}</VaultCheck>;
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar />
